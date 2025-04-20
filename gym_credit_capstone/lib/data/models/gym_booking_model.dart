4d69db8c50c70f4fa817fb3e887807133ecea1c4
sjw79790 <sjw79790@gmail.com>
2025-04-20 23:29:50 +0900

    Update gym_booking_model.dart
    예약시 status를 항상 true로 수정, 예약 불가능한 값일 때 예약 기능 취소

--- gym_credit_capstone/lib/data/models/gym_booking_model.dart
+++ gym_credit_capstone/lib/data/models/gym_booking_model.dart
@@ -1,5 +1,3 @@
-//import 'package:cloud_firestore/cloud_firestore.dart';
-
 class GymBookingModel {
   final Map<String, String> sportsTranslation = {
     "축구장": "SCCR",
@@ -12,28 +10,6 @@ class GymBookingModel {
     "수영장": "PL",
     "골프장": "GLF"
   };
-
-  /*Future<void> saveReservationToFirestore(
-      String gymId, String gymAbbreviation, String userId, DateTime selectedDate, String selectedTime, String sportsSummary, int price) async {
-    final formattedDate = "${selectedDate.toLocal()}".split(' ')[0];
-    final formattedCreateTime = selectedDate.toLocal().toIso8601String();
-    final translatedSportsSummary = translateSportsSummary(sportsSummary);
-
-    DocumentReference reservationDoc = FirebaseFirestore.instance.collection('reservations').doc("${formattedDate}_${selectedTime}_${gymId}_${translatedSportsSummary}_${userId}");
-    await reservationDoc.set({
-      'ceatetime': formattedCreateTime, // 생성 시간
-      'date': formattedDate, // 날짜
-      'gymAbbrevation': gymAbbreviation, // 체육관 약어
-      'gymid': gymId, // 체육관 ID
-      'sports': {
-        'price': price, // 가격
-        'sportname': translatedSportsSummary // 운동 종목
-      },
-      'status': true, // 상태
-      'time': selectedTime, // 선택한 시간
-      'userid': userId, // 사용자 ID
-    });
-  }*/
 
   String translateSportsSummary(String sportsSummary) {
     List<String> sportsList = sportsSummary.split(', ');
